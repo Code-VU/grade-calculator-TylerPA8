706d83fc6896c1d758d96f75071046ab2635a46e
Tyler Paneitz <Tyler.Paneitz@vu.com>
2022-01-25 13:07:52 -0600

    Re-uploaded assignment.

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,8 +2,23 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
-
-    hrs = float(input("Enter score:"))
+    try:
+        score = float(input("Enter score:"))
+    except:
+        score = 1.1
+        #changes score when it finds error. Will throw bad score in following if statement.
+    if score > 1 or score < 0:
+        print("Bad score")
+    elif score >= 0.9:
+        print ("A")
+    elif score >= 0.8:
+        print ("B")
+    elif score >= 0.7:
+        print ("C")
+    elif score >= 0.6:
+        print ("D")
+    else:
+        print ("F")
 
     # end assignment
 
